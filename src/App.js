1b23f41bb5880731a273df56f0a5ebad9c750a68
Sarthak Joshi <sarthakjoshi2496@gmail.com>
2020-12-10 14:45:58 +0530

    raw version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
+import ShowCards from './components/ShowCards';
 
-function App() {
+
+const DivContainer = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: none;
+  font-family: monospace;
+  justify-content: center;
+  align-items: center;
+  width: 80vw;
+  margin: 30px auto;
+`;
+
+const DivResult = styled.div`
+  border: 2px solid black;
+  padding: 5px;
+  height: 700px;
+`;
+
+const App = () => {
+
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [items, setItems] = useState([]); // items is kinda like memory which holds visited item
+
+  const handleScore = (e) => {
+    const val = e.currentTarget;
+    // check if currently clicked item is in items array, if it is- bestscore->score and score->0, if not, setItems to items+clicked item and increment the score
+    if (items.includes(val)){
+      setBestScore(winScore => (score > bestScore ? score : bestScore)); //set bestscore(winScore) to score only if score > bestscore
+      setScore(0);
+      setItems([]); //make items array empty to play next chances
+    } else {
+      setItems(items => ([...items, val]));
+      setScore(score + 1);
+    }
+  }
+ 
+  useEffect(() => {
+
+    const shuffleCards = (array) => { 
+      for (let i=array.length-1; i>0; i--) { // Fisher-Yates shuffle-The idea is to walk the array in the reverse order and swap each element with a random one before it
+        let j=Math.floor(Math.random()* (i+1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+    const displayShuffle = () => {
+      const cardsArray = Array.from(cards); // cardsArray is array that contains nodes
+      cardsArray.forEach((card) => parent.removeChild(card));
+      shuffleCards(cardsArray);
+      cardsArray.forEach((card) => parent.appendChild(card));
+    };
+
+    const parent = document.querySelector('#parent'); // parent is container
+    const cards = parent.querySelectorAll('.cards'); //cards is a nodelist
+    cards.forEach((card)=> card.addEventListener('click', displayShuffle)); //add event listener on nodelist not array
+    
+    return () => {
+      cards.forEach((card)=> card.removeEventListener('click', displayShuffle)); //add event listener on nodelist not array
+    };
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DivContainer>
+      <ShowCards computeScore={handleScore} />
+      <DivResult>
+        <h2> Mind Game </h2>
+        <p> Current Score: {score} </p>
+        <p> Best Score: {bestScore} </p>
+      </DivResult>
+    </DivContainer>
   );
-}
+};
 
 export default App;
