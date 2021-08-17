7d4a4aade7e8fa40a9188ecad38d770a7d23cb11
Sarthak Joshi <sarthakjoshi2496@gmail.com>
2021-08-18 00:24:30 +0530

    img changes

--- src/components/ShowCards.js
+++ src/components/ShowCards.js
@@ -41,7 +41,7 @@ const ShowCards = (props) => {
             birth: "763 AD",
         },
         {
-            img: "https://static.wikia.nocookie.net/vikingstv/images/f/f4/LagerthaSeason6.jpeg/revision/latest/scale-to-width-down/327?cb=20201209232405",
+            img: "https://i.pinimg.com/originals/30/87/ca/3087ca8227ba8e3ad7540ea74740d3ab.jpg",
             name: "Lagertha",
             birth: "763 AD",
         },
